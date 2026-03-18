ada75dc2d1e523102665efc4832dcfa01b375c53
adile <adil@gmail>
2026-03-18 13:51:12 +0100

    packeages added

--- Info/info.py
+++ Info/info.py
@@ -1,2 +1,3 @@
 # uv sync when first run creates virtual environmet, and later it should be used after each new packages installations
 # hover over function to see it's explanation
+# add ipykernel and ipywidgets modules to support jupyter notebook files
